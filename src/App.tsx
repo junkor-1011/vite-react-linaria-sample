b51a8dd818bd684407676d6071660ae561343106
junkor-1011 <tomoyukizxy@gmail.com>
2023-06-05 23:43:18 +0900

    example

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { css } from '@linaria/core'
 import './App.css'
 
 function App() {
@@ -16,7 +17,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className={css`color: blue;`}>
+        Vite + React
+      </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
